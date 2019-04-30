93a1f985a24d1a9a95b2d05cccbe4296e1f863e0
steven <stevensocio@gmail.com>
2019-04-30 14:39:28 -0500

    avances 29/04/2019

--- formularioLS/assets/js/app.js
+++ formularioLS/assets/js/app.js
@@ -36,13 +36,48 @@ function agregarTweet(e){
     //añadir el tweet a la lista
     listaTweets.appendChild(li);
     console.log(tweet);
+
+    //Añadir a Local Storage
+    agregarTweetLocalStorage(tweet);
 }
 
 function borrarTweet(e){
     e.preventDefault();
+    //Delegation 
     if(e.target.className === 'borrar-tweet'){
-        console.log('Diste click en eliminar')
+        console.log('Diste click en eliminar');
+        //eliminar con .remove()
+        let con = confirm(`deseas eliminar ${e.target.className}`);
+        if(con == true){
+            console.log(e.target.parentElement.remove());
+        }
+        
     }else{
         console.log('diste click en otra parte');
     } 
+}
+
+
+//Agrega tweet a local storage
+function agregarTweetLocalStorage(tweet){
+    let tweets;
+    //obtener tweets
+    tweets = obtenerTweesLocalStorage();
+    //añadir nuevo tweet}
+    tweets.push(tweet);
+    //Convertir de String a array para local Storage
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+    //agregar al local Storage
+    //localStorage.setItem('tweets', tweet);
+}
+
+function obtenerTweesLocalStorage(){
+    let tweets;
+    //Revisando los valores de local storage
+    if(localStorage.getItem('tweets') === null){
+        tweets = [];
+    }else{
+        tweets = JSON.parse(localStorage.getItem('tweets'));
+    }
+    return tweets;
 }
