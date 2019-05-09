ade902e8c2b3e9220757326debe23fcdc19c61e1
steven <stevensocio@gmail.com>
2019-05-09 16:57:41 -0500

    avances 09/05/2019

--- pruebas/app.js
+++ pruebas/app.js
@@ -98,6 +98,7 @@ console.log(arreglos1);
 console.log(arreglos2);
 */
 
+/*
 //Prototypes
 function Cliente(nombre,saldo){
     this.nombre = nombre;
@@ -119,6 +120,171 @@ Cliente.prototype.tipoCliente = function(edad){
 
 }
 
+//const cliente1 = new Cliente('Pedro', 100);
+
+//console.log(cliente1);
+//console.log(cliente1.tipoCliente());
+
+//Prototipo que imprimes saldo y nombre
+Cliente.prototype.nombreClienteSaldo = function(){
+    return `nombre: ${this.nombre}, Tu saldo es de ${this.saldo}, Tipo Cliente ${this.tipoCliente()}`
+}
+
+//Retirar Saldo
+Cliente.prototype.retirarSaldo = function(retiro){
+    return this.saldo -= retiro;
+}
+
+const cliente1 = new Cliente('Pedro', 100);
+const cliente2 = new Cliente('coco', 600);
+const cliente3 = new Cliente('zeus', 1100);
+
+cliente2.retirarSaldo(300);
+
+console.log(cliente1.nombreClienteSaldo());
+console.log(cliente2.nombreClienteSaldo());
+console.log(cliente3.nombreClienteSaldo());
+
+*/
+
+/*
+//Heredar Prototypes a otro Objeto
+function Cliente(nombre,saldo){
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+
+//Prototipo que imprimes saldo y nombre
+Cliente.prototype.nombreClienteSaldo = function(){
+    return `nombre: ${this.nombre}, Tu saldo es de ${this.saldo}`
+}
+
 const cliente1 = new Cliente('Pedro', 100);
 
 console.log(cliente1);
+
+//Banca para empresas
+function Empresa(nombre, saldo,telefono,tipo){
+    Cliente.call(this, nombre, saldo);
+
+    this.telefono = telefono;
+    this.tipo = tipo;
+}
+
+//Heredar el prototype de nombreClienteSaldo
+Empresa.prototype = Object.create(Cliente.prototype);
+
+const empresa = new Empresa('Vertical', 59000000, 322123123,'desarrollo');
+
+console.log(empresa.nombreClienteSaldo());
+
+*/
+
+/*
+//Object create
+const Cliente = {
+    imprimirSaldo : function(){
+        return `hola ${this.nombre} Tu saldo es ${this.saldo}`
+    },
+    retirarSaldo: function(retiro){
+        return this.saldo -= retiro; 
+    }
+}
+
+//Crear el objeto
+const mary = Object.create(Cliente);
+mary.nombre = 'Mary';
+mary.saldo = 1000;
+
+mary.retirarSaldo(300);
+
+console.log(mary.imprimirSaldo());
+*/
+
+
+/*
+//Creando Una clase EcmaScript 6
+class Cliente{
+    constructor(nombre,apellido,saldo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.saldo = saldo;
+    }
+
+    imprimirSaldo(){
+        return `Hola ${this.nombre}, tu saldo es de: ${this.saldo}`
+    }
+
+    tipoCliente(){
+        let tipo;
+        if(this.saldo > 10000){
+            tipo = 'Gold';
+        }else if(this.saldo > 5000){
+            tipo = 'Platinum'
+        }else{
+            tipo = 'Normal';
+        }
+        return tipo;
+    }
+
+    retirarSaldo(retiro){
+        return this.saldo -= retiro;
+    }
+    
+    static bienvenido(){
+        return `Bienvenido al Cajero`
+    }
+
+}
+
+const maria = new Cliente('Maria', 'perez', 11000);
+maria.retirarSaldo(3000);
+
+//console.log(maria);
+//console.log(maria.imprimirSaldo());
+
+console.log(Cliente.bienvenido());
+*/
+
+
+//herencia en classes EcmaScript 6
+class Cliente{
+    constructor(nombre,saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    imprimirSaldo(){
+        return `Hola ${this.nombre}, tu saldo es de: ${this.saldo}`
+    }
+    
+    static bienvenido(){
+        return `Bienvenido al Cajero`
+    }
+
+}
+
+const cliente = new Cliente('pedro', 1000);
+
+console.log(cliente.imprimirSaldo());
+
+class Empresa extends Cliente{
+    constructor(nombre,saldo,telefono,tipo){
+        //va hacia el constructor Padre
+        super(nombre, saldo);
+        //no existen en el constructor padre
+        this.telefono = telefono;
+        this.tipo = tipo;
+    }
+
+    static bienvenido(){
+        return `Bienvenido al Cajero para Empresas`
+    }
+}
+
+const empresa = new Empresa('Empresa1', 10000, 12123312,'construcion');
+
+//console.log(empresa);
+
+console.log(empresa.imprimirSaldo());
+console.log(Empresa.bienvenido());
