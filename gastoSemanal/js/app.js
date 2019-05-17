3eb96180acba6506f716ebf9af2d14c4e1694bea
steven <stevensocio@gmail.com>
2019-05-17 17:04:52 -0500

    avances 17/05/2019

--- gastoSemanal/js/app.js
+++ gastoSemanal/js/app.js
@@ -1,6 +1,7 @@
 //Variables
 const presupuestoUsuario = prompt('Cual es tu presupuesto Semanal?');
 //console.log(presupuestoUsuario);
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
 //Clases
@@ -20,8 +21,47 @@ let cantidadPresupuesto;
  //Clase de interfaz maneja todo lo relacionado a el html
  class Interfaz{
      insertarPresupuesto(cantidad){
-        const presupuestoSpan = document.querySelector('span #total');
-        const restanSpan = document.querySelector('span #restante');
+        const presupuestoSpan = document.querySelector('span#total');
+        const restanSpan = document.querySelector('span#restante');
+
+        //Insertar al HTML
+        presupuestoSpan.innerHTML = `${cantidad}`;
+        restanSpan.innerHTML = `${cantidad}`;
+     }
+
+     imprimirMensaje(mensaje,tipo){
+         const divMensaje = document.createElement('div');
+         divMensaje.classList.add('text-center','alert');
+         if(tipo === 'error'){
+             divMensaje.classList.add('alert-danger');
+         }else{
+             divMensaje.classList.add('alert-success');
+         }
+         divMensaje.appendChild(document.createTextNode(mensaje)); 
+
+         //Insertar en DOM
+         document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+         //Quitar el alert despues de 3 segundos
+         setTimeout(function(){
+            document.querySelector('.primario .alert').remove();
+            formulario.reset();
+         },3000);
+     }
+     //Insertar los gastos a la lista
+     agregarGastoListado(nombreGasto, cantidadGasto){
+         const gastoListado = document.querySelector('#gastos ul');
+         //Crear un Li
+         const li = document.createElement('li');
+         li.className ='list-group-item d-flex justify-content-between aling-itens-center';
+
+         li.innerHTML = `
+            ${nombreGasto}
+            <span class="badge badge-primary badge-pill">$ ${cantidadGasto}</span>
+         `;
+
+         //Insertar al HTML
+         gastoListado.appendChild(li);
      }
  }
 
@@ -41,4 +81,32 @@ document.addEventListener('DOMContentLoaded', function(){
         const ui = new Interfaz();
         ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
     }
+});
+
+
+formulario.addEventListener('submit',function(e){
+    e.preventDefault();
+    //console.log('enviado');
+
+    //Lerr del formulario de Gatos
+    const nombreGasto = document.querySelector('#gasto').value;
+    const cantidadGasto = document.querySelector('#cantidad').value;
+
+    //Instanciar la Interfas
+    const ui = new Interfaz();
+
+    //Comprobar que los cmapos esten vacios
+    if(nombreGasto === '' || cantidadGasto === ''){
+        //console.log('hubo un error');
+        //2 parametros: mensaje y tipo
+        ui.imprimirMensaje('Hubo un error', 'error');
+    }else{
+        //console.log('El gasto se agrego');
+
+        //Insertar en el HTML
+        ui.imprimirMensaje('Correcto', 'correcto');
+        ui.agregarGastoListado(nombreGasto,cantidadGasto);
+
+    }
+
 })
