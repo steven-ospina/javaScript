29e97f65b3133a75d927c2cc9c04c0b82be035e7
steven <stevensocio@gmail.com>
2019-05-31 00:12:02 -0500

    practica expresiones regulares

---
+++ expresiones_regulares/app.js
@@ -0,0 +1,24 @@
+/*
+const cedula    = 'V-11122233'// --- cadena a probar
+const regExp    = RegExp(/[BE]-[0-9]{1,8}/) // --- sin comillas
+const resultado = regExp.test( cedula )
+//const Rrr = RegExp( /[VE] /);
+//--
+console.log ( resultado );
+*/
+/*
+const h = document.getElementById("titulo").className;
+
+console.log(h);
+
+let manz = document.getElementById('manzana').textContent;
+
+
+const buscar = RegExp(/\W/g);
+
+
+
+console.log(buscar.test(manz));
+
+*/
+
