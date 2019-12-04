9ed62c7deb011b80725ab9cb5f3f4cf89e8551eb
socio <stevensocio@gmail.com>
2019-12-04 16:19:52 -0500

    code js clean

--- javaScriptMDN/practica/relog-analogico/main.js
+++ javaScriptMDN/practica/relog-analogico/main.js
@@ -1,24 +1,60 @@
 function relog() {
     const getTime = new Date();
-    let hora = getTime.getHours();
+    //crea las variables del relog
+    const relogDigital = {
+        horaDigital: getTime.getHours(),
+        minutosDigitla: getTime.getMinutes(),
+        segundosDigital: getTime.getSeconds()
+    }
+    //crea el relog y lo inserta en el DOM
+    const time = `${relogDigital.horaDigital}:${relogDigital.minutosDigitla}:${relogDigital.segundosDigital}`;
+    document.querySelector(".relogDigital").innerHTML = time;
+    document.title = `Reloges:${time}`;
+}
+
+function horas() {
+    //obtiene la hora 
+    const getTime = new Date();
+    const hora = {
+        hour: getTime.getHours()
+    }
+    //compara la hora y le resta 12 para poder mostrar la hora exacta en el DOM
+    if (hora.hour >= 12) {
+        hora.hour - 12
+    }
+    //calculo para poder rotar la hora en el DOM
+    const hours = (hora.hour / 12 * 360);
+    const horasDom = document.querySelector(".horas");
+    horasDom.style.transform = `rotate(${hours}deg)`;
+}
+
+function minutos() {
+    //Obtiene los minutos
+    const getTime = new Date();
     const minutos = getTime.getMinutes();
-    const segundos = getTime.getSeconds();
-    const time = `${hora}:${minutos} ${segundos}`;
-    const divs = document.querySelector(".hora").innerHTML = time;
-    let seconds = (segundos / 60 * 360);
-    const segundosDom = document.querySelector(".segundos");
-    segundosDom.style.transform = `rotate(${seconds}deg)`;
+    //Calculo para poder rotar los minutis en el DOM
     const minutes = (minutos / 60 * 360);
     const minutosDom = document.querySelector(".minutos")
     minutosDom.style.transform = `rotate(${minutes}deg)`;
-    if(hora >= 12){
-        hora - 12
-    }
-    const hours = (hora / 12 * 360);
-    const horasDoms = document.querySelector(".horas");
-    horasDoms.style.transform =  `rotate(${hours}deg)`;
-    // console.log(hours);
 }
 
-setInterval(relog,1000);
+function segundos() {
+    //Obtiene los segundos
+    const getTime = new Date();
+    const segundos = getTime.getSeconds();
+    //Calculo para poder rotar los segundos en el DOM
+    let seconds = (segundos / 60 * 360);
+    const segundosDom = document.querySelector(".segundos");
+    segundosDom.style.transform = `rotate(${seconds}deg)`;
+}
 
+/*
+Para que el relog-digital y el relog-analogico puedan
+funcionar en el DOM. 
+*/
+setInterval(() => {
+    relog();
+    horas();
+    minutos();
+    segundos();
+}, 1000);
